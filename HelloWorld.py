f1fa134473c119ba9fe32dd98bf0b93a106eb1a6
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-09-27 20:14:36 +0800

    HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,6 +9,6 @@ age_next_year = age + 1
 pi=3894.767
 
 print("Hello, "+ name + "!")
-print(f"My age next year is " + {(age_next_year)} )
-print(f"Pi value is",{pi})
+print(f"My age next year is {(age_next_year)}")
+print(f"Pi value is,{pi}")
 
