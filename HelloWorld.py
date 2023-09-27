e782bb4ff4c8fdea52e91d8b6c0fb225ca387fbb
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-09-27 08:47:36 +0800

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+
+name = input("What is your name? ")
+age_input = int(input("What is your age? "))
+
+age = int(age_iput)
+
+age_next_year = age + 1
+
+pi=3894.767
+
+print("Hello, "+ name + "!")
+print(f"My age next year is " + {(age_next_year)} )
+print(f"Pi value is",{pi})
+
