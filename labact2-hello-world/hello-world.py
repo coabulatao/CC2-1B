659d14626e68c44a798b0e6350553c68eb95250f
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-09-29 18:42:29 +0800

    Add files via upload

---
+++ labact2-hello-world/hello-world.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+age_input = int(input("What is your age? "))
+age = int(age_input)
+
+age_next_year = age + 1
+
+pi=3894.767
+modulo = 5 % 3
+
+print("Hello, "+ name + "!")
+print(f"your age next year is {(age_next_year)}")
+print(f"Pi value is{pi}")
+print(f"5 times 3 is {modulo}")
