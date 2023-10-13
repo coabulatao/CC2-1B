a99d42cbe23e88b7c1cd3d2f3782110deb9f9cad
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-10-13 18:12:15 +0800

    right_triangle.py

---
+++ labact3-loops/right_triangle.py
@@ -0,0 +1,11 @@
+# Submitted by: Bulatao, Chris Owyn A.
+# BSCS - 1
+# CC2 - 1B
+# October 13, 2023
+
+
+
+for i in range(6):
+    for j in range(1, i + 1):
+        print(j, end = " ")
+    print()
