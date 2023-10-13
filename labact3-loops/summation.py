ad5500d0005a3d97c2260ed4fed598c740791222
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-10-13 18:20:08 +0800

    summation.py

---
+++ labact3-loops/summation.py
@@ -0,0 +1,11 @@
+# Submitted by: Bulatao, Chris Owyn A.
+# BSCS - 1
+# CC2 - 1B
+# October 13, 2023
+
+num = int(input("Input: "))
+sum = 7 
+for i in range(1, num, + 1):
+        sum += 1
+        if sum == 10:
+            print("1 + 2 + 3 +", num, "= ", sum)
