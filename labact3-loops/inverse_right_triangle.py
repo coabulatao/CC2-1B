62ec66aaacf39204febd85e916686c3b9c5f949a
Haruuuuuuuuu <145944603+coabulatao@users.noreply.github.com>
2023-10-13 18:17:44 +0800

    inverse_right_triangle.py

---
+++ labact3-loops/inverse_right_triangle.py
@@ -0,0 +1,10 @@
+# Submitted by: Bulatao, Chris Owyn A.
+# BSCS - 1
+# CC2 - 1B
+# october 13, 2023
+
+
+for i in range(5, 0, -1):
+    for j in range(1, i, + 1):
+        print(j, end =" ")
+    print()
